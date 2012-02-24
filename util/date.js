3b16f22607b52f238963a9dd8ff04001b131faf5
mangix <maqh1988@gmail.com>
2012-02-24 17:30:21 +0800

    aaa

--- util/date.js
+++ util/date.js
@@ -4,16 +4,63 @@
  * date.js
  */
 
-(function($, undefined){
-    var date = $.single(function(){
+(function ($, undefined) {
+    var fix, date;
+    fix = function (number, length) {
+        number = "000000000000000000000" + number;
+        return number.substr(number.length - length);
+    };
+
+    date = $.single(function () {
         return {
-            parseDate: function(str, format){
-                format = format ? format : "yyyy-MM-dd hh:mm:ss";
+            parse:function (param) {
+                var args;
+                if(typeof param ==="string"){
+                    args = param.split(/-|\/|\s|:/).join(',');
+                }else{
+                    args = [].join.call(arguments,',');
+                }
+                return eval("(new Date(" + args + "))");
             },
-            formatDate: function(format){
+            format:function (formatStr, date) {
+                //format the Date object to the formatStr
+                var year , month, day, hour, minute, second;
+                year = date.getFullYear();
+                month = date.getMonth() + 1;
+                day = date.getDate();
+                hour = date.getHours();
+                minute = date.getMinutes();
+                second = date.getSeconds();
+                return formatStr.replace(/(yyyy)|(M+)|(d+)|(h+)|(m+)|(s+)/g, function (str, y, m, d, h, mnt, s) {
+                    if (y) {
+                        return fix(year, y.length);
+                    }
+                    if (m) {
+                        return fix(month, m.length);
+                    }
+                    if (d) {
+                        return fix(day, d.length);
+                    }
+                    if (h) {
+                        return fix(hour, h.length);
+                    }
+                    if (mnt) {
+                        return fix(minute, mnt.length);
+                    }
+                    if (s) {
+                        return fix(second, s.length);
+                    }
+                });
             }
         };
     });
-	
-	
+
+
+    $.nameSpace.pack("Mallan.urtil.date", date);
+    $.extendCustom({
+        name:"date",
+        cls:date,
+        constructType:"single"
+    });
+
 })(Mallan);
